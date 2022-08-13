b2353200cdcd3bf1b273794f6885323a68efea3a
mariamzhamakochyan <107346258+mariamzhamakochyan@users.noreply.github.com>
2022-08-14 01:15:06 +0400

    Create palindrome_number.py

---
+++ palindrome_number.py
@@ -0,0 +1,11 @@
+x = int(input("Enter the number: "))
+temp = x 
+rev = 0
+while x > 0:
+      dig = x % 10
+      rev = rev * 10 + dig
+      x = x // 10
+if temp == rev:
+   print("True")
+else:
+   print("False")
