e167da9e79f83dee8f0c226ebe831e75234ba558
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-08-20 01:30:10 +0400

    Update Smallest_multiple.py

--- Smallest_multiple.py
+++ Smallest_multiple.py
@@ -1,14 +1,12 @@
-num = 0 
+def is_div(num):
+    for i in range(2, 21):
+        if num % i != 0:
+            return False
+    return True
+    
+num = 20
 while True:
-    num += 20
-    num_mult = 0
-    for i in range(20,10,-1):
-        if num % i == 0:
-            num_mult += 1
-        else:
-            break
-    if num_mult == 10:
-        break
-    if num % 1000000 == 0:
-        print(num / 1000000)
+    if is_div(num):
+       break
+    num +=20
 print(num)
